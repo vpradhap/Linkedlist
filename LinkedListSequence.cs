52f67b634b84e6919f991306f28868f2645e522e
vpradhap <pradhapvenkat@gmail.com>
2022-05-24 22:13:36 +0530

    Added : Adding value at the front of linked list

---
+++ LinkedListSequence.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        //constructor
+        public Node(int item)
+        {
+            data = item;
+            next = null;
+        }
+    }
+    public class LinkedListSequence
+    { 
+        public Node head;
+ 
+        public void Adding(int newValue)
+        {
+            Node newNode = new Node(newValue);
+            newNode.next = head;
+            head = newNode;
+            Console.WriteLine(newNode.data+" Inserted into the list in front ");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("\nLinked List Sequence\n");
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("\nLinked List is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(temp.data);
+                    if(temp.next != null)
+                    {
+                        Console.Write(" -> ");
+                    }
+                    temp = temp.next;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Let's create a simple Linked List of 56,30 and 70");
+﻿using LinkedList;
+
+Console.WriteLine("\nLet's create a simple Linked List of 56,30 and 70\n");
+
+LinkedListSequence call = new LinkedListSequence();
+call.Adding(70);
+call.Adding(30);
+call.Adding(56);
+call.Display();
+Console.ReadKey();
